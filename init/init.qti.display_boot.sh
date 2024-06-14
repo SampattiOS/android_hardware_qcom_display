0d49b10c1422a8c4a938c00434ab98557dd98f45
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-06-14 10:24:29 +0530

    init: Disable HWC large comp perf hint
    Disables the PID Affine feature thus preventing perf HAL from messing with
    HWC and SF cpu sets, since we've affined both to foreground already.
    Thanks to Jake for this idea.
    
    Change-Id: I8edd5289692a6a47992e6a6dc809dbf237a42761

--- init/init.qti.display_boot.sh
+++ init/init.qti.display_boot.sh
@@ -46,7 +46,7 @@ case "$target" in
         setprop vendor.display.target.version 4
         setprop vendor.display.enable_posted_start_dyn 2
         setprop vendor.display.enable_allow_idle_fallback 1
-        setprop vendor.display.enable_perf_hint_large_comp_cycle 1
+        setprop vendor.display.enable_perf_hint_large_comp_cycle 0
         setprop vendor.display.enable_rotator_ui 1
         setprop vendor.display.enable_spec_fence 1
         setprop vendor.display.thermal.version 1
@@ -66,7 +66,7 @@ case "$target" in
         setprop vendor.display.target.version 5
         setprop vendor.display.enable_posted_start_dyn 2
         setprop vendor.display.enable_allow_idle_fallback 1
-        setprop vendor.display.enable_perf_hint_large_comp_cycle 1
+        setprop vendor.display.enable_perf_hint_large_comp_cycle 0
         setprop vendor.display.enable_rotator_ui 1
         setprop vendor.display.enable_spec_fence 1
         setprop vendor.display.thermal.version 1
@@ -88,7 +88,7 @@ case "$target" in
         setprop vendor.display.target.version 5
         setprop vendor.display.enable_posted_start_dyn 2
         setprop vendor.display.enable_allow_idle_fallback 1
-        setprop vendor.display.enable_perf_hint_large_comp_cycle 1
+        setprop vendor.display.enable_perf_hint_large_comp_cycle 0
         setprop vendor.display.enable_rotator_ui 1
         setprop vendor.display.enable_spec_fence 1
         setprop vendor.display.thermal.version 1
@@ -117,7 +117,7 @@ case "$target" in
         setprop vendor.display.target.version 5
         setprop vendor.display.enable_posted_start_dyn 2
         setprop vendor.display.enable_allow_idle_fallback 1
-        setprop vendor.display.enable_perf_hint_large_comp_cycle 1
+        setprop vendor.display.enable_perf_hint_large_comp_cycle 0
         setprop vendor.display.enable_rotator_ui 1
         setprop vendor.display.enable_spec_fence 1
         setprop vendor.display.thermal.version 1
@@ -139,7 +139,7 @@ case "$target" in
         setprop vendor.gralloc.use_dma_buf_heaps 1
         setprop vendor.display.enable_posted_start_dyn 2
         setprop vendor.display.enable_allow_idle_fallback 1
-        setprop vendor.display.enable_perf_hint_large_comp_cycle 1
+        setprop vendor.display.enable_perf_hint_large_comp_cycle 0
         setprop vendor.display.enable_rotator_ui 1
         setprop vendor.display.enable_spec_fence 0
         setprop vendor.display.thermal.version 1
@@ -163,7 +163,7 @@ case "$target" in
         setprop vendor.gralloc.use_dma_buf_heaps 1
         setprop vendor.display.enable_posted_start_dyn 2
         setprop vendor.display.enable_allow_idle_fallback 1
-        setprop vendor.display.enable_perf_hint_large_comp_cycle 1
+        setprop vendor.display.enable_perf_hint_large_comp_cycle 0
         setprop vendor.display.enable_rotator_ui 1
         setprop vendor.display.enable_spec_fence 0
         setprop vendor.display.thermal.version 1
@@ -176,7 +176,7 @@ case "$target" in
         setprop vendor.gralloc.use_dma_buf_heaps 1
         setprop vendor.display.enable_posted_start_dyn 2
         setprop vendor.display.enable_allow_idle_fallback 1
-        setprop vendor.display.enable_perf_hint_large_comp_cycle 1
+        setprop vendor.display.enable_perf_hint_large_comp_cycle 0
         setprop vendor.display.enable_rotator_ui 1
         setprop vendor.display.enable_spec_fence 0
         setprop vendor.display.thermal.version 1
@@ -191,7 +191,7 @@ case "$target" in
         setprop vendor.gralloc.use_dma_buf_heaps 1
         setprop vendor.display.enable_posted_start_dyn 2
         setprop vendor.display.enable_allow_idle_fallback 1
-        setprop vendor.display.enable_perf_hint_large_comp_cycle 1
+        setprop vendor.display.enable_perf_hint_large_comp_cycle 0
         setprop vendor.display.enable_rotator_ui 1
         setprop vendor.display.enable_spec_fence 0
         setprop vendor.display.thermal.version 1
@@ -209,13 +209,13 @@ case "$target" in
         # Set property for lahaina
         setprop vendor.display.target.version 1
         setprop vendor.display.enable_posted_start_dyn 2
-        setprop vendor.display.enable_perf_hint_large_comp_cycle 1
+        setprop vendor.display.enable_perf_hint_large_comp_cycle 0
         setprop vendor.display.enable_allow_idle_fallback 1
         ;;
         450)
         # Set property for shima
         setprop vendor.display.target.version 2
-        setprop vendor.display.enable_perf_hint_large_comp_cycle 1
+        setprop vendor.display.enable_perf_hint_large_comp_cycle 0
         setprop vendor.display.enable_posted_start_dyn 1
         setprop vendor.display.enable_qsync_idle 1
         setprop vendor.display.enable_allow_idle_fallback 1
